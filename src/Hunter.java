c6a08da641dd1df6f2662867a668f74e8544bac2
Nuhas R <nuhasyt07@gmail.com>
2025-01-13 21:18:21 -0500

    Finished GTH-1

--- src/Hunter.java
+++ src/Hunter.java
@@ -130,7 +130,7 @@ public class Hunter {
 
         for (String item : kit) {
             if (item != null) {
-                printableKit += item + space;
+                printableKit += Colors.PURPLE+item+Colors.RESET + space;
             }
         }
         return printableKit;
@@ -140,7 +140,7 @@ public class Hunter {
      * @return A string representation of the hunter.
      */
     public String infoString() {
-        String str = hunterName + " has " + gold + " gold";
+        String str = hunterName + " has " + Colors.YELLOW + gold +Colors.RESET + " gold";
         if (!kitIsEmpty()) {
             str += " and " + getInventory();
         }
