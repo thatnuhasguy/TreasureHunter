53bc8a3d2451b632b9c60b879639d2183c0fdf92
BT_1N3_17 <sample@bths.edu>
2025-01-14 10:14:25 -0500

    Finished GTH-4

--- src/Shop.java
+++ src/Shop.java
@@ -13,6 +13,7 @@ public class Shop {
     private static final int MACHETE_COST = 6;
     private static final int HORSE_COST = 12;
     private static final int BOAT_COST = 20;
+    private static final int BOOTS_COST = 5;
 
     // static variables
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -86,6 +87,7 @@ public class Shop {
         str += "Machete: " + MACHETE_COST + " gold\n";
         str += "Horse: " + HORSE_COST + " gold\n";
         str += "Boat: " + BOAT_COST + " gold\n";
+        str += "Boots: " + BOOTS_COST + " gold\n";
         return str;
     }
 
@@ -149,6 +151,8 @@ public class Shop {
             return HORSE_COST;
         } else if (item.equals("boat")) {
             return BOAT_COST;
+        } else if (item.equals("boots")) {
+            return BOOTS_COST;
         } else {
             return 0;
         }
--- src/Town.java
+++ src/Town.java
@@ -122,16 +122,18 @@ public class Town {
      */
     private Terrain getNewTerrain() {
         double rnd = Math.random();
-        if (rnd < .2) {
+        if (rnd < .16666) {
             return new Terrain("Mountains", "Rope");
-        } else if (rnd < .4) {
+        } else if (rnd < .33333) {
             return new Terrain("Ocean", "Boat");
-        } else if (rnd < .6) {
+        } else if (rnd < .5) {
             return new Terrain("Plains", "Horse");
-        } else if (rnd < .8) {
+        } else if (rnd < .66666) {
             return new Terrain("Desert", "Water");
-        } else {
+        } else if (rnd < .83333) {
             return new Terrain("Jungle", "Machete");
+        } else {
+            return new Terrain("Marsh", "Boots");
         }
     }
 
